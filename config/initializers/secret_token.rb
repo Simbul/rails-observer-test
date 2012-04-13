7402e9cb2912eb9fcbe592aa699e0ee352e68451
Alessandro Morandi <alessandro@dachisgroup.com>
2012-04-13 11:16:01 +0100

    Initialized repo with a new Rails application (generated via the 'rails new' command).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ObserverTestInit::Application.config.secret_token = '79475fd24298fc277acd4b22fde30010778b2fdbdce40241f0125961857de7d3b904d35f757aad4705dab5c03b07985bb5ca5dfeff1032eff2fa6607ef6d57e5'
